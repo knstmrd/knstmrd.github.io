8ccb6e9de9f6a84b91f1d4dddbd28f6cf4e388d0
George Oblapenko <kunstmord@kunstmord.com>
2020-06-25 19:10:26 -0500

    updated verbs

--- assets/js/dv.js
+++ assets/js/dv.js
@@ -10,9 +10,38 @@ verbs_list = [{
     'present': ['schließe', 'schließt', 'schließt', 'schließen', 'schließt', 'schließen'],
     'preterite': ['schloss', 'schlossest', 'schloss', 'schlossen', 'schlosst', 'schlossen'],
     'perfect': 'geschlossen',
-    'translation': 'to shut, close down, lock'}
+    'translation': 'to shut, close down, lock'},
+    {
+        'inf': 'denken',
+        'present': ['denke', 'denkst', 'denkt', 'denken', 'denkt', 'denken'],
+        'preterite': ['dachte', 'dachtest', 'dachte', 'dachten', 'dachtet', 'dachten'],
+        'perfect': 'gedacht',
+        'translation': 'to think',
+        'preposition': 'an',
+        'prepcase': 'Acc.',
+    },
+        {
+        'inf': 'achten',
+        'present': ['achte', 'achtest', 'achtet', 'achten', 'achtet', 'achten'],
+        'preterite': ['achtete', 'achtetest', 'achtetete', 'achteten', 'achtetet', 'achteten'],
+        'perfect': 'geachtet',
+        'translation': 'to care about, pay attention to, keep an eye on',
+        'preposition': 'auf',
+        'prepcase': 'Acc.',
+    }
+    
 ];
+    // {
+    //     'inf': ,
+    //     'present': [],
+    //     'preterite': [],
+    //     'perfect': ,
+    //     'translation': ,
+    //     'preposition': ,
+    //     'prepcase': ,
+    // }
 
+ich_list = ['ich', 'du', 'sie/er', 'wir', 'ihr', 'Sie'];
 
 var len_verbs_list = verbs_list.length;
 var content = window.document.getElementById("container");
@@ -23,19 +52,43 @@ function generate() {
     form = Math.floor(Math.random() * 3);
 
     if (form == 0) {
-        content.innerHTML = 'Infinitiv: ' + verbs_list[verb_id]['inf'];
+        content.innerHTML = '<u>Infinitiv</u>: ' + verbs_list[verb_id]['inf'];
     }
     else if (form == 1) {
-        content.innerHTML = 'Perfekt: ' + verbs_list[verb_id]['perfect'];
+        content.innerHTML = '<u>Perfekt</u>: ' + verbs_list[verb_id]['perfect'];
     }
     else {
-        content.innerHTML = 'Translation: ' + verbs_list[verb_id]['translation'];
+        content.innerHTML = '<u>Translation</u>: ' + verbs_list[verb_id]['translation'];
     }
 }
 
 function show() {
-    console.log('showing', verb_id);
-    content.innerHTML = 'Infinitiv: ' + verbs_list[verb_id]['inf'] + '<br>';
-    content.innerHTML += 'Perfekt: ' + verbs_list[verb_id]['perfect'] + '<br>';
-    content.innerHTML += 'Translation: ' + verbs_list[verb_id]['translation'];
+    content.innerHTML = '<u>Infinitiv</u>: ' + verbs_list[verb_id]['inf'] + '<br>';
+
+    content.innerHTML += '<u>Present</u>: ';
+    for (i=0; i<6; i++) {
+        content.innerHTML += ich_list[i] + ' ' + verbs_list[verb_id]['present'][i];
+        if (i != 5) {
+            content.innerHTML += ' | ';
+        }
+    }
+    content.innerHTML += '<br>';
+
+    content.innerHTML += '<u>Perfekt</u>: ' + verbs_list[verb_id]['perfect'] + '<br>';
+
+    content.innerHTML += '<u>Preterite</u>: ';
+    for (i=0; i<6; i++) {
+        content.innerHTML += ich_list[i] + ' ' + verbs_list[verb_id]['preterite'][i];
+        if (i != 5) {
+            content.innerHTML += ' | ';
+        }
+    }
+    content.innerHTML += '<br>';
+
+    if ("preposition" in verbs_list[verb_id]) {
+        content.innerHTML += '<u>With preposition</u>: ' + verbs_list[verb_id]['inf'] + ' ';
+        content.innerHTML += verbs_list[verb_id]['preposition'] + ' <i>(' + verbs_list[verb_id]['prepcase'] + ' case)</i><br>';
+    }
+
+    content.innerHTML += '<u>Translation</u>: ' + verbs_list[verb_id]['translation'];
 }
